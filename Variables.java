47f9ce8f107109f0f2ee669228527ab47b1319c3
Somar <sbrasomar@gmail.com>
2025-12-17 16:38:42 +0200

    Practiced arth expressions

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        
+        // This is your new "Main" for practicing variables
+        int x = 123;
+        double y = 3.14;
+        boolean isJavaFun = true;
+        char symbol = '#';
+        String phrase = "Hello Nvidia";
+        
+        System.out.println(phrase + "! x is " + x);
+    }
+}
