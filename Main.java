f67a994f109e860fba411c9ade33d521c0b6c4df
Somar <sbrasomar@gmail.com>
2025-12-17 16:15:55 +0200

    Added user input functionality using Scanner

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
+import java.util.Scanner; 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Congratulations! Your Java environment is ready.");
-        System.out.println("Next stop: Nvidia.");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old.");
+        
+        scanner.close();
+
+
+        
+
+
+
+       
     }
 }
