186df894f2b4ca79dc81d58c1c53859d91662d32
Somar <sbrasomar@gmail.com>
2025-12-17 15:53:56 +0200

    Initial commit: First Java program on WSL

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Congratulations! Your Java environment is ready.");
+        System.out.println("Next stop: Nvidia.");
+    }
+}
