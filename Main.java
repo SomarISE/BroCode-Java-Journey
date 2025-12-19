247689b447a2566873e1810a0e89a305e473f3bc
Somar <sbrasomar@gmail.com>
2025-12-19 13:37:06 +0200

    Add Car class with copy constructor and test Main

--- Main.java
+++ Main.java
@@ -2,18 +2,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name? ");
-        String name = scanner.nextLine();
-        System.out.println("How old are you? ");
-        int age = scanner.nextInt();
-        scanner.nextLine();
+    
 
-        
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age + " years old.");
-        
-        scanner.close();
+        Car myCar = new Car("Toyota", "Camry", 2020, 24000);
+        System.out.println("My car details: " + myCar.toString());
 
 
         
